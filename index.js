7bd2c9884c3e8b626354070f6479c1ff7c7fb109
Damon <damon.nicholas.allen@live.com>
2022-08-16 21:53:45 -0400

    On task 4

--- index.js
+++ index.js
@@ -194,9 +194,24 @@ Use the game function below to do the following:
 RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors | Or there's a tie
 */
 
-function game(user, computer){
-  /*add your code here*/
+function game(user, computer) {
+  if (user === computer) {
+    return "it's a tie";
+  } else if (user == 'rock' && computer == 'paper') {
+    return "you lose!";
+  } else if (user == 'rock' && computer == "scissors") {
+    return "you win!";
+  } else if (user == 'paper' && computer == 'rock') {
+    return "you win!";
+  } else if (user == 'paper' && computer == 'scissors') {
+    return "you lose!";
+  } else if (user == 'scissors' && computer == 'rock') {
+    return "you lose!";
+  } else if (user == 'scissors' && computer == 'paper') {
+    return "you win!";
+  }
 }
+
 
 
 
